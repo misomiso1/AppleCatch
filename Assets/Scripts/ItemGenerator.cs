ca9057a665225143019ae473b2c3db821f924b3b
mipurin <jibumeili78@gmail.com>
2024-03-12 09:50:15 +0900

    UI追加

--- Assets/Scripts/ItemGenerator.cs
+++ Assets/Scripts/ItemGenerator.cs
@@ -9,6 +9,12 @@ public class ItemGenerator : MonoBehaviour
     float span = 1.0f;
     float delta = 0;
     int ratio=2;
+    float speed=-0.03f;
+    public void SetParameter(float span,float speed,int ratio){
+        this.span=span;
+        this.speed=speed;
+        this.ratio=ratio;
+    }
 
     void Update()
     {
@@ -26,6 +32,7 @@ public class ItemGenerator : MonoBehaviour
             float x = Random.Range(-1,2);
             float z = Random.Range(-1,2);
             item.transform.position=new Vector3(x,4,z);
+            item.GetComponent<ItemController>().dropSpeed=this.speed;
         }
         
     }
